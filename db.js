55041907e8f330655eb057f9316724a641656dca
Jeff Seaman <46333921+jeffreyjseamanppu@users.noreply.github.com>
2025-02-04 20:08:02 -0500

    Update db.js
    This points to the render environment (prod)

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "grapes",
-    host: "localhost",
+    user: "jseaman",
+    password: "6rKRAV1z4OBSntzILbVrxDiPSLxRtnRD",
+    host: "dpg-cuhbffi3esus73ch0bug-a",
     database: "paspirits",
     port: 5432,
 });
