2afde983d67b83936b2e18bd90d0937f7e44f101
Jeff Seaman <46333921+jeffreyjseamanppu@users.noreply.github.com>
2025-03-10 22:49:37 -0400

    Update db.js
    Updated the new configuration

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "jseaman",
-    password: "6rKRAV1z4OBSntzILbVrxDiPSLxRtnRD",
-    host: "dpg-cuhbffi3esus73ch0bug-a",
-    database: "paspirits",
+    user: "jeffreyjseaman",
+    password: "kmbkpIXlU3EMe2iB1qS5TWrs7f2nngXw",
+    host: "dpg-cucmet1opnds73aj7dag-a",
+    database: "wine_xxgq",
     port: 5432,
 });
 
