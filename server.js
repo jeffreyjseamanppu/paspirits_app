d63c1eccbb659e5930abd2b3bf2ac05189444a21
Jeff Seaman <professorjeffreyjseaman@gmail.com>
2025-03-25 12:18:47 -0400

    Added fix to resolve CWe-200 Snyk code fix

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require("express");
+const helmet = require('helmet');
 const cors = require("cors");
 
 //Routes
 const accountsRoutes = require("./src/accounts/routes");
 
 const app = express();
-const port = 9021;
+
+const port = 9022;
 
 app.use(express.json());
+app.use(helmet()); //Sets HTTP headers for security like removing X-Powered-By, enabling CSP
 app.use(cors({
     origin: '*'
 }));
 
 //route
-app.get("/", (req, res)=> {
+app.get("/", (req, res) => {
     res.send("Who will the Super Bowl??");
 })
 
